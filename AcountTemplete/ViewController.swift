da949224f68abe2ad8458a6e87795508202b04a8
AquaQAQ <1632259373@qq.com>
2019-07-19 11:33:44 +0800

    Initial Commit

---
+++ AcountTemplete/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AcountTemplete
+//
+//  Created by Wallance on 2019/7/19.
+//  Copyright © 2019 Wallance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
